908863428945fd8e57c10e1e990db1adca393f97
tuilahuan <vanhuana4k56@gmail.com>
2024-08-27 16:27:13 +0700

    push

--- services/apiServices.js
+++ services/apiServices.js
@@ -6,12 +6,11 @@ const postUploadImage = (image) => {
   return axios.post("https://5579.mekongai.io/upload_image/", formData);
 };
 
-const postGenerateImage = (nameImage, background, outfit, sex) => {
-  return axios.post("https://5588.mekongai.io/style_snap", {
+const postGenerateImage = (nameImage, background, style) => {
+  return axios.post("https://public.mekongai.com/style_snap", {
     url: `https://5579.mekongai.io/images/uploads/${nameImage}`,
     background,
-    outfit,
-    sex,
+    style,
   });
 };
 export { postUploadImage, postGenerateImage };
